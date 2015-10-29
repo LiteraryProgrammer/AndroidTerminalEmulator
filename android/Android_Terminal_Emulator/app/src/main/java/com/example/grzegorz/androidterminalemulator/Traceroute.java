cc6a4763d57a12866b330a02de9803e0b9224708
Grzegorz Pietrusza <gpietrus@akamai.com>
2015-10-29 19:18:19 +0100

    add rtts to traceroute

--- android/Android_Terminal_Emulator/app/src/main/java/com/example/grzegorz/androidterminalemulator/Traceroute.java
+++ android/Android_Terminal_Emulator/app/src/main/java/com/example/grzegorz/androidterminalemulator/Traceroute.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,6 +22,33 @@ import java.util.regex.Pattern;
 
 //todo: live output
 public class Traceroute extends ExtraCommand {
+
+    private class RTT {
+        String ip;
+        String pingRTTRegexp = "^PING .*\\n64 bytes from .* time=(.*) ms\\n64 bytes from .* time=(.*) ms\\n64 bytes from .* time=(.*) ms\\n\\n.*\\n.*\\n.*\\n$";
+        Pattern pingRTTPattern = Pattern.compile(pingRTTRegexp);
+        public RTT(String ip) {
+            this.ip = ip;
+        }
+        public float[] getRTT() throws IOException, InterruptedException {
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec("ping -c 3 " + this.ip);
+            process.waitFor();
+            String response = IOUtils.toString(process.getInputStream());
+            Matcher pingRTTMatcher = pingRTTPattern.matcher(response);
+            if(pingRTTMatcher.matches()){
+                float time1 = Float.parseFloat(pingRTTMatcher.group(1));
+                float time2 = Float.parseFloat(pingRTTMatcher.group(2));
+                float time3 = Float.parseFloat(pingRTTMatcher.group(3));
+                return new float[] {time1, time2, time3};
+            } else {
+                return new float[] {};
+            }
+        }
+
+    }
+
+    //todo: allow fqdn as input ip
 
     private Boolean finishedFlag = false;
     private int PING_MAX_TTL = 64;
@@ -67,21 +95,33 @@ public class Traceroute extends ExtraCommand {
         }
     }
 
-    //todo: osobny watek ktory wykonuje pingi i pisze do jednego duzego streama, i jego analizujemy
 
+    //notes:
     @Override
     protected Object doInBackground(Object[] params) {
 
         final PipedInputStream totalIs = new PipedInputStream();
+        Boolean measureRTT = false;
         String dstIP = null;
         String[] args = cmd.split(" ");
-        if(args.length == 2) {
-            dstIP = args[1];
-        } else {
-            publishProgress("usage: traceroute ip");
+
+        if(args.length < 2) {
+            publishProgress("usage: traceroute ip [-t]\n-t - prints rtts");
             return null;
         }
+
+        if(args.length < 3) {
+            dstIP = args[1];
+        }
+        else if(args.length == 3 && args[2].equals("-t")) {
+            measureRTT = true;
+        }
+
+
+        //todo: add mesasure rtt arg
+
         final String finalDstIP = dstIP;
+        final Boolean finalMeasureRTT = measureRTT;
         new Thread(new Runnable() { //todo: move to sepeerate class?
             @Override
             public void run() {
@@ -99,7 +139,6 @@ public class Traceroute extends ExtraCommand {
                         process.waitFor();
 
                         is = process.getInputStream();
-                        //todo: rest of streams?
 
                         String response = IOUtils.toString(is);
                         String ip;
@@ -116,11 +155,26 @@ public class Traceroute extends ExtraCommand {
                         } else if (finalResponseMatcher.matches()) {
                             ip = finalResponseMatcher.group(1);
                         } else {
-                            out.write("invalid response\n".getBytes());
+                            out.write("invalid response\n".getBytes()); //todo? when?
                             continue;
                         }
 
-                        out.write((ip != null ? ip + "\n" : "no response\n").getBytes());
+                        if(ip == null) {
+                            out.write("no response\n".getBytes());
+                            continue;
+                        }
+
+                        StringBuilder rrtsStrBuilder = new StringBuilder();
+                        if(finalMeasureRTT) {
+                            if (ip != null) {
+                                float rtts[] = new RTT(ip).getRTT();
+                                for (int i = 0; i < rtts.length; i++) {
+                                    rrtsStrBuilder.append(rtts[i] + " ms\t");
+                                }
+                            }
+                        }
+                        String rrtsStr = finalMeasureRTT ? rrtsStrBuilder.toString() : "";
+                        out.write((ip + "\t" + (rrtsStr.length() > 0 ? rrtsStr : "") + "\n").getBytes());
 
                         if (ip != null && ip.equals(finalDstIP)) {
                             break;
